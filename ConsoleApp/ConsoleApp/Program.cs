e2e8de669780573acb95f590d8b4386ccff26dce
sranjeet612 <sranjeet612@gmail.com>
2022-03-06 18:47:57 +0530

    Modified code in Program.cs file

--- ConsoleApp/ConsoleApp/Program.cs
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("My first .Net Core Console Application!!!");
         }
     }
 }
