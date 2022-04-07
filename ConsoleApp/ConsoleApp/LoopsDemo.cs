1a362726f67f46798574de3d22aa9a2b1f689801
sranjeet612 <sranjeet612@gmail.com>
2022-04-07 09:16:38 +0530

    added classes for loops

---
+++ ConsoleApp/ConsoleApp/LoopsDemo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class LoopsDemo
+    {
+        public static void Main()
+        {
+            int x = 1;
+            // while loop 
+            while (x<=10)
+            {
+                Console.WriteLine($"the value of x is:{x}");
+                x++;
+            }
+
+            // do while loop
+
+            do
+            {
+                Console.WriteLine($"do while loop is running with value of x:{x}");
+            } while (x<10);
+
+            // for loop
+            int y = 0;
+            for (int z=10; y<10 && z<=10; y++,z--)
+            {
+                Console.WriteLine($"The value of y is :{y} and Z is:{z}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ ConsoleApp/ConsoleApp/Ternarydemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Ternarydemo
+    {
+        public static void Main()
+        {
+            int x=new System.Random().Next(-10,10);
+            string result=(x < 0) ? "x is negative" : "x is positive";
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+
+    }
+}
