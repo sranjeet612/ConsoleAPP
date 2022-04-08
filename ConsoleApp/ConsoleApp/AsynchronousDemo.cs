585a8937b38cc71f473b625a63a2cd88013f4a17
sranjeet612 <sranjeet612@gmail.com>
2022-04-08 18:49:24 +0530

    Aysnc and await method demo

---
+++ ConsoleApp/ConsoleApp/AsynchronousDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class AsynchronousDemo
+    {
+
+        private async void Test()
+        {
+            Console.WriteLine("Async method is started.");
+            await Task.Delay(1000);
+            Console.WriteLine("Async method is completed.");
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Main Method is started....");
+            new AsynchronousDemo().Test();
+            new AsynchronousDemo().Test3();  
+            Console.WriteLine("Main Method is Completed....");
+            Console.ReadLine();
+        }
+
+        Task<string> Test2()
+        {
+            return Task.Run<string>(() =>
+            {
+                Thread.Sleep(1000);
+                return "Running Task method with Lembda Expression";
+            });
+        }
+
+        async void Test3()
+        {
+            string result = await Test2();
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ ConsoleApp/ConsoleApp/GenericDemo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class GenericDemo
+    {
+        const string Name = "Generic Class Demo";
+        readonly string Email;
+        readonly string Password;
+
+        public GenericDemo()
+        {
+            this.Email = "sranjeet612@gmail.com";
+            this.Password = "123";
+        }
+        public static void Main()
+        {   
+            GenericDemo demo = new GenericDemo();
+            demo.Swap(10, 20);
+            demo.Swap<string>("Ranjeet", "Singh");
+            demo.Swap<bool>(true,false);
+          
+            Console.WriteLine(GenericDemo.Name);
+        }
+
+        public void Swap(int a, int b)
+        {
+            Console.WriteLine($"The Value of a is:{a} and b is{b}");
+            int Temp = a;
+            a = b;
+            b = Temp;
+
+            Console.WriteLine($"The Value of a is:{a} and b is{b}");
+
+        }
+
+        //-Generic method
+
+        public void Swap<T>(T x, T y)
+        {
+            Console.WriteLine($"The value of x:{x} and y:{y}");
+            T temp = x;
+            x = y;
+            y = temp;
+            Console.WriteLine($"The value of x:{x} and y:{y}");
+        }
+    }
+
+    public class GenericClassDemo<T>
+    {
+        public T Add<T>(T x, T y)
+        {
+            return x;
+        } 
+    }
+}
+
+
+
+
+
